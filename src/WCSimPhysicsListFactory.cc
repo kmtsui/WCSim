8014e2f7fd73e53868e24343ee3451cc4714c9d6
kmtsui <a122014@hotmail.com>
2023-01-06 11:22:37 +0900

    Construct custom optical physics class with CoatedDielecetricDielectric surface

--- src/WCSimPhysicsListFactory.cc
+++ src/WCSimPhysicsListFactory.cc
@@ -146,7 +146,7 @@ void WCSimPhysicsListFactory::InitializeList(){
       RegisterPhysics(elem);
     }
     G4cout << "RegisterPhysics: OpticalPhysics" << G4endl; 
-    RegisterPhysics(new G4OpticalPhysics());
+    RegisterPhysics(new WCSimOpticalPhysics());
     
     // Add Radioactive Decay:
     G4cout << "RegisterPhysics: RadioactiveDecayPhysics" << G4endl;
